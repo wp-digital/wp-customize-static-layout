26eb66c6a694d7f826816d7d49dbccee15658ad9
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2017-12-28 17:16:20 +0200

    previwer scroll for new sections

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,12 +5,15 @@
     var api = wp.customize;
 
     $(function () {
-        api.section.each(function (section) {
+        var onExpanded = function (section) {
             if (section.id.replace(/\[\d+]$/, '').lastIndexOf(settings.panel, 0) === 0) {
                 section.container.on('expanded', function (section) {
                     api.previewer.send(settings.panel, section);
                 }.bind(null, section.id));
             }
-        });
+        };
+
+        api.section.each(onExpanded);
+        api.section.bind('add', onExpanded);
     });
 })(jQuery, window.wp, window.customizeStaticLayout);
