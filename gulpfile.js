18f7836b018cb5cd81499c193db149d2a60fcb42
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2018-01-02 15:35:01 +0200

    fix gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ let cleanCSS = require('gulp-clean-css');
 let uglify = require('gulp-uglify');
 let rename = require('gulp-rename');
 
-gulp.task('css', () => gulp.src('./assets/css/*.css')
+gulp.task('css', () => gulp.src(['./assets/css/*.css', '!./assets/css/*.min.css'])
     .pipe(cleanCSS())
     .pipe(rename(path => {
         path.basename += '.min';
     }))
     .pipe(gulp.dest('./assets/css')));
-gulp.task('js', () => gulp.src('./assets/js/*.js')
+gulp.task('js', () => gulp.src(['./assets/js/*.js', '!./assets/js/*.min.js'])
     .pipe(uglify())
     .pipe(rename(path => {
         path.basename += '.min';
